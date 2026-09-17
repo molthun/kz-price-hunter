51a902c49ad8e8f387148984d36bfcd872da4a27
molthun <bogdanets.n@gmail.com>
2026-09-17 20:21:33 +0500

    feat(ui): display official store logos and tooltips instead of emojis (v2.5.5)

--- version.py
+++ version.py
@@ -2,7 +2,7 @@
 KZ Price Hunter - Version Information
 """
 
-__version__ = "2.5.4"
+__version__ = "2.5.5"
 __app_name__ = "KZ Price Hunter"
 __release_date__ = "2026-09-17"
 __author__ = "Advanced Agentic Coding"
